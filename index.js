f5a51f22a19e06fc3d2b9f430c45a08223cf2acf
Rajiv Narayana Singaseni <rajivnarayana@gmail.com>
2018-11-05 19:05:18 +0530

    invert position system for dom manager.

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 const GRID_SIZE = 4;
 
-const UP = 0;
-const RIGHT = 1;
-const DOWN = 2;
-const LEFT = 3;
-
 function Board(inputManager, domManager) {
     this.grid = this.placeRandom(new Grid(GRID_SIZE));
     this.domManager = domManager;
@@ -20,22 +15,27 @@ Board.prototype.move = function (direction) {
 }
 
 Board.prototype.placeRandom = function(grid) {
-    grid.setItem(0, GRID_SIZE-1, 2);
-    grid.setItem(GRID_SIZE/2, 1, 2);
+    grid.setItem(0, 3, 2);
+    grid.setItem(2, 1, 2);
     return grid;
 }
 
+const UP = 0;
+const RIGHT = 1;
+const DOWN = 2;
+const LEFT = 3;
+
 Board.prototype.play = function (grid, direction) {
     //TODO: Update the grid. moving in the given `direction`
-    //Ex grid.setItem(0,0, 4);
-    console.log(direction);
+    // Uncomment below line for example.
+    // grid.setItem(0, 1, 4);
 }
 
 Board.prototype.createCells = function() {
     this.domManager.clearBoard();
     for(let i=0; i< GRID_SIZE; i++) {
         for(let j=0; j< GRID_SIZE; j++) {
-            this.domManager.addTile({x : i,y: j, value: this.grid.getItem(i, j) || null});
+            this.domManager.addTile({x : j, y: i, value: this.grid.getItem(i, j) || null});
         }
     }
 }
@@ -50,10 +50,15 @@ Grid.prototype.getItem = function(row, col) {
 
 Grid.prototype.setItem = function(row, col, val) {
     this.items[row][col] = val;
+    this.debug();
 }
 
 Grid.prototype.clone = function() {
     const newGrid = new Grid();
     newGrid.items = new Array(GRID_SIZE).fill(0).map((ele, row) => new Array(GRID_SIZE).fill(0).map((ele, col) => this.getItem(row, col)));
     return newGrid;
+}
+
+Grid.prototype.debug = function() {
+    console.log(this.items.map(item => item.join("\t")).join("\n"));
 }
